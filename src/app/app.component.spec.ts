fd3caeb09b106eb2b481abebc69758f8ae9ffe3f
R.Ide <6502263+rafaelide@users.noreply.github.com>
2020-09-10 11:12:40 -0300

    remove ng generated content

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('interviews');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('interviews app is running!');
-  });
 });
